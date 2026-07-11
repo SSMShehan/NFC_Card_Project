4b0043cf1bce8a5b6ecc9f6044071b3618dddd4a
SSMShehan <shehanmadusanka775@gmail.com>
2026-07-12 02:12:54 +0530

    Mount CartDrawer in Providers so it actually shows up

--- packages/web/context/Providers.tsx
+++ packages/web/context/Providers.tsx
@@ -5,6 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './AuthContext';
 import { ThemeProvider } from './ThemeContext';
 import { CartProvider } from './CartContext';
+import { CartDrawer } from '../components/CartDrawer';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "demo-google-client-id.apps.googleusercontent.com";
@@ -15,6 +16,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         <AuthProvider>
           <CartProvider>
             {children}
+            <CartDrawer />
           </CartProvider>
         </AuthProvider>
       </ThemeProvider>
